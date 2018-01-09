6120e5b2802fdd28c0744b0dd18b0fe48c745121
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-01-09 13:44:39 +0100

    compression deactivated

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 rm -Rf /usr/local/data/backup.jnl
 curl \
           --data-urlencode "file=/usr/local/data/backup.jnl" \
-          --data-urlencode "compress=true" \
+          --data-urlencode "compress=false" \
           --data-urlencode "block=true" \
           http://localhost:8080/blazegraph/backup  
 

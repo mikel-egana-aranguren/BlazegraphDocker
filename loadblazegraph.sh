92f2bc5915b33d0cccc1309b41f39fea84f6518e
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-06-06 11:20:06 +0200

    start tomcat

--- loadblazegraph.sh
+++ loadblazegraph.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-/opt/apache-tomcat-8.0.52/bin/catalina.sh stop
-
-sleep 20
-
 cp -rf /usr/local/data/blazegraph.jnl /opt/blazegraph.jnl
 
 /opt/apache-tomcat-8.0.52/bin/catalina.sh run &
